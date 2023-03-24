ec98849f29e6cdd611172234616e75f4d36e04d5
JakobEdvardsson <jakob.edvardsson@outlook.com>
2023-03-24 11:39:11 +0100

    Adding some test features
    Creating a Toast and a button that moves the user to SettingsActivity

--- app/src/main/java/com/example/liftingstack/MainActivity.java
+++ app/src/main/java/com/example/liftingstack/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.liftingstack;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void launchSettings(View v){
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
+
+    public void toast(View v) {
+        Toast.makeText(this, "Toast", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/example/liftingstack/SettingsActivity.java
@@ -0,0 +1,14 @@
+package com.example.liftingstack;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
