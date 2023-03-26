527dac22dd0e77dd966fd12f7d979dacb945c488
JakobEdvardsson <jakob.edvardsson@outlook.com>
2023-03-26 16:45:34 +0200

    Fixed some unnecessary code in ExerciseHistory  and created class ExerciseDataController

--- app/src/main/java/com/example/liftingstack/Entity/ExerciseData.java
+++ app/src/main/java/com/example/liftingstack/Entity/ExerciseData.java
@@ -6,22 +6,34 @@ import java.util.List;
 
 public class ExerciseData {
     private Exercise exercise;
-    private HashMap<Integer, List<Double>> excerciseData;
+    private HashMap<Integer, List<Double>> exerciseData;
     //key = set, list[0] = rep, List[1] = weigh
 
     public ExerciseData(Exercise exercise) {
         this.exercise = exercise;
-        excerciseData = new HashMap<>();
+        exerciseData = new HashMap<>();
     }
 
-    public HashMap<Integer, List<Double>> getExcerciseData() {
-        return excerciseData;
+    public HashMap<Integer, List<Double>> getExerciseData() {
+        return exerciseData;
     }
 
     public void addExcerciseData(int set, double rep, double weigh) {
         List<Double> data = new ArrayList<>();
         data.add(rep);
         data.add(weigh);
-        excerciseData.put(set, data);
+        exerciseData.put(set, data);
+    }
+
+    public Exercise getExercise() {
+        return exercise;
+    }
+
+    public void setExercise(Exercise exercise) {
+        this.exercise = exercise;
+    }
+
+    public void setExerciseData(HashMap<Integer, List<Double>> exerciseData) {
+        this.exerciseData = exerciseData;
     }
 }
--- app/src/main/java/com/example/liftingstack/Entity/ExerciseHistory.java
+++ app/src/main/java/com/example/liftingstack/Entity/ExerciseHistory.java
@@ -1,21 +1,33 @@
 package com.example.liftingstack.Entity;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 public class ExerciseHistory {
-    private List<ExerciseData> exerciseDataList = new ArrayList<>();
+    private HashMap<Exercise, List<ExerciseData>> exerciseDataList = new HashMap<>();
 
-    public List<ExerciseData> getExerciseDataList() {
-        return exerciseDataList;
-    }
     public void addExerciseDataToList(ExerciseData exerciseData) {
-        exerciseDataList.add(exerciseData);
+        //Kollar först från hashmappen om det specifika Exercise objektet finns, sedan lägger till det i arraylistan
+        //Om det inte finns så skapas ett nytt element i hashmappen och en arraylist läggs till.
+        if (exerciseDataList.containsKey(exerciseData.getExercise())){
+            Objects.requireNonNull(exerciseDataList.get(exerciseData.getExercise())).add(exerciseData);
+        }else {
+            ArrayList<ExerciseData> data = new ArrayList<>();
+            data.add(exerciseData);
+            exerciseDataList.put(exerciseData.getExercise(), data);
+        }
+
     }
     public void removeExerciseDataFromList(ExerciseData exerciseData) {
-        exerciseDataList.remove(exerciseData);
+        Objects.requireNonNull(exerciseDataList.get(exerciseData.getExercise())).remove(exerciseData);
     }
-    public void setExerciseDataList(List<ExerciseData> exerciseDataList) {
+
+    public HashMap<Exercise, List<ExerciseData>> getExerciseDataList() {
+        return exerciseDataList;
+    }
+    public void setExerciseDataList(HashMap<Exercise, List<ExerciseData>> exerciseDataList) {
         this.exerciseDataList = exerciseDataList;
     }
 }
