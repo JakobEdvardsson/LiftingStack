38d0ed2dd62e7265f0bfedc3f3114236df75c54d
BenniHansson <98157948+BenniHansson@users.noreply.github.com>
2023-03-29 15:20:30 +0200

    linked java variable username to xml textfield

--- app/src/main/java/com/example/liftingstack/ProfileActivity.java
+++ app/src/main/java/com/example/liftingstack/ProfileActivity.java
@@ -1,14 +1,20 @@
 package com.example.liftingstack;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class ProfileActivity extends AppCompatActivity {
+    private String username = "BNI";
+    private TextView usernameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        findViewById(R.id.usernameTextView);
+        usernameTextView.setText(username);
     }
 }
