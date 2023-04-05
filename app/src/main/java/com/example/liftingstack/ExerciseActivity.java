a6c78a09d14ba86b43b5b2159a8bbb9781f551b8
JakobEdvardsson <jakob.edvardsson@outlook.com>
2023-04-05 14:57:28 +0200

    Adding listener on Exercise buttons

--- app/src/main/java/com/example/liftingstack/ExerciseActivity.java
+++ app/src/main/java/com/example/liftingstack/ExerciseActivity.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.liftingstack.Entity.ExerciseInstructions;
 import com.example.liftingstack.Entity.Program;
@@ -14,7 +15,7 @@ import com.example.liftingstack.Entity.Program;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ExerciseActivity extends AppCompatActivity {
+public class ExerciseActivity extends AppCompatActivity implements ExerciseRecyclerViewInterface{
 
     private List<ExerciseInstructions> exerciseInstructions = new ArrayList<>();
 
@@ -25,7 +26,7 @@ public class ExerciseActivity extends AppCompatActivity {
 
         RecyclerView recyclerView = findViewById(R.id.listExercise);
         setUpExerciseList();
-        ExerciseRecyclerViewAdapter exerciseAdapter = new ExerciseRecyclerViewAdapter(this, exerciseInstructions);
+        ExerciseRecyclerViewAdapter exerciseAdapter = new ExerciseRecyclerViewAdapter(this, exerciseInstructions, this);
         recyclerView.setAdapter(exerciseAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
@@ -41,5 +42,11 @@ public class ExerciseActivity extends AppCompatActivity {
         {
             exerciseInstructions.add(new ExerciseInstructions("Exercise " + i , "Hard exercise"));
         }
+    }
+
+    @Override
+    public void onExerciseClick(int position) {
+        System.out.println("XXXXXXXXXXXXXXXX " + position);
+        Toast.makeText(getApplicationContext(),"Hello Javatpoint",Toast.LENGTH_SHORT).show();
     }
 }
