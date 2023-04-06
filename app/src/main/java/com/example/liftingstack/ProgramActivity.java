340f922a7dd934642616512dd9b746606a60d2b8
gihedenalexander <giheden.alexander@gmail.com>
2023-04-06 16:50:52 +0200

    continued adding functionality in the program activity

--- app/src/main/java/com/example/liftingstack/ProgramActivity.java
+++ app/src/main/java/com/example/liftingstack/ProgramActivity.java
@@ -7,17 +7,28 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.view.View;
 
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.liftingstack.Entity.Program;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProgramActivity extends AppCompatActivity
+public class ProgramActivity extends AppCompatActivity implements RecyclerViewInterface
 {
     private List<Program> programs = new ArrayList<>();
+    ImageView editView;
+    ImageView confirmView;
+    ImageView deleteView;
+    Button backButton;
+    ImageView addIcon;
+    TextView addDescription;
+    ImageView undoView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -27,9 +38,11 @@ public class ProgramActivity extends AppCompatActivity
 
         RecyclerView recyclerView = findViewById(R.id.listPrograms);
         setUpProgramsList();
-        ProgramRecyclerViewAdapter programAdapter = new ProgramRecyclerViewAdapter(this, programs);
+        ProgramRecyclerViewAdapter programAdapter = new ProgramRecyclerViewAdapter(this, programs, this);
         recyclerView.setAdapter(programAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
 
     }
 
@@ -45,5 +58,60 @@ public class ProgramActivity extends AppCompatActivity
         {
             programs.add(new Program("Program " + i , "Back and biceps"));
         }
+    }
+
+    public void makeVisible(View view)
+    {
+        editView = findViewById(R.id.imageViewEdit);
+        editView.setVisibility(View.VISIBLE);
+
+        confirmView = findViewById(R.id.imageViewConfirm);
+        confirmView.setVisibility(View.VISIBLE);
+
+        deleteView = findViewById(R.id.imageViewDelete);
+        deleteView.setVisibility(View.VISIBLE);
+
+        undoView = findViewById(R.id.cancelImageView);
+        undoView.setVisibility(View.VISIBLE);
+
+
+        undoView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+
+            public void onClick(View view)
+            {
+                backButton.setVisibility(View.VISIBLE);
+                addIcon.setVisibility(View.VISIBLE);
+                addDescription.setVisibility(View.VISIBLE);
+
+                editView.setVisibility(View.INVISIBLE);
+                confirmView.setVisibility(View.INVISIBLE);
+                deleteView.setVisibility(View.INVISIBLE);
+                undoView.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+
+    @Override
+    public void makeInvisible(View view)
+    {
+        backButton = findViewById(R.id.backButton);
+        backButton.setVisibility(View.INVISIBLE);
+
+        addIcon = findViewById(R.id.addIcon);
+        addIcon.setVisibility(View.INVISIBLE);
+
+        addDescription = findViewById(R.id.addDescriptionText);
+        addDescription.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public void onItemClick(Program program)
+    {
+        //Intent intent = new Intent(this, SelectedProgramActivity.class);
+        //startActivity(intent);
+        //Toast.makeText(getApplicationContext(),"HejEHjehe",Toast.LENGTH_SHORT).show();
+
     }
 }
---
+++ app/src/main/java/com/example/liftingstack/RecyclerViewInterface.java
@@ -0,0 +1,16 @@
+package com.example.liftingstack;
+
+
+import android.view.View;
+
+
+import com.example.liftingstack.Entity.Program;
+
+public interface RecyclerViewInterface
+{
+    void onItemClick(Program program);
+
+    void makeVisible(View view);
+
+    void makeInvisible(View view);
+}
