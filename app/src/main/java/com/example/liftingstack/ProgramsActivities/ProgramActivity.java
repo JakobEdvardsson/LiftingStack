cadf5a3aa7b2003db56ec63c947c392c90ad3e6a
gihedenalexander <giheden.alexander@gmail.com>
2023-04-14 09:39:57 +0200

    Update ProgramActivity.java

--- app/src/main/java/com/example/liftingstack/ProgramsActivities/ProgramActivity.java
+++ app/src/main/java/com/example/liftingstack/ProgramsActivities/ProgramActivity.java
@@ -6,23 +6,15 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.util.Log;
-import android.view.View;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.example.liftingstack.Entity.Program;
-import com.example.liftingstack.MainActivity;
 import com.example.liftingstack.R;
 
 import java.util.ArrayList;
