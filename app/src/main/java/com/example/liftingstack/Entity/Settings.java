41cc8354c13897aa6877098663079ba2c023bbc1
Oliver Berggren <oliverberggren00@gmail.com>
2023-04-15 11:52:53 +0200

    Update Settings.java
    Co-Authored-By: Ebbish <128602352+Ebbski@users.noreply.github.com>

--- app/src/main/java/com/example/liftingstack/Entity/Settings.java
+++ app/src/main/java/com/example/liftingstack/Entity/Settings.java
@@ -13,7 +13,7 @@ public class Settings extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings_activity);
+        setContentView(R.layout.activity_settings);
         if (savedInstanceState == null) {
             getSupportFragmentManager()
                     .beginTransaction()
