2cf220ee74c3cc1fd2c75b5f3578275d48efee9b
Oliver Berggren <oliverberggren00@gmail.com>
2023-04-15 15:14:48 +0200

    Is finished with the bar chart which displays the number of training sessions

--- app/src/main/java/com/example/liftingstack/Entity/BarChartCount.java
+++ app/src/main/java/com/example/liftingstack/Entity/BarChartCount.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 
 import com.example.liftingstack.R;
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
@@ -13,7 +14,7 @@ import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 
-public class BarChart extends AppCompatActivity {
+public class BarChartCount extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +24,15 @@ public class BarChart extends AppCompatActivity {
         BarChart barChart = findViewById(R.id.barChart);
 
         ArrayList<BarEntry> bar = new ArrayList<>();
-        bar.add(new BarEntry(2022, 10));
-        bar.add(new BarEntry(2022, 11));
-        bar.add(new BarEntry(2022, 12));
-        bar.add(new BarEntry(2022, 13));
-        bar.add(new BarEntry(2022, 5));
-        bar.add(new BarEntry(2022, 6));
-        bar.add(new BarEntry(2022, 7));
-        bar.add(new BarEntry(2022, 8));
+        bar.add(new BarEntry(2023-1, 10));
+        bar.add(new BarEntry(2023-2, 11));
+        bar.add(new BarEntry(2023-3, 12));
+        bar.add(new BarEntry(2023-4, 13));
+        bar.add(new BarEntry(2023-5, 5));
+        bar.add(new BarEntry(2023-6, 6));
+        bar.add(new BarEntry(2023-7, 7));
+        bar.add(new BarEntry(2023-8, 8));
+        //ändra ev x - axel (från 2023-1 till 1, eller skriv det på etta annat sätt)
 
         BarDataSet barDataSet = new BarDataSet(bar, "Number of training sessions");
         barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
@@ -42,7 +44,7 @@ public class BarChart extends AppCompatActivity {
         barChart.setFitBars(true);
         barChart.setData(barData);
         barChart.getDescription().setText("Bar");
-
+        barChart.animateY(2000);
 
     }
 }
