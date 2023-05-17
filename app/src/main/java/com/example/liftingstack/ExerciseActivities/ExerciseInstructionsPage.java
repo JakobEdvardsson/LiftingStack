16f82a4ca303c38c58dd157aa9014b1b51f37535
JakobEdvardsson <jakob.edvardsson@outlook.com>
2023-05-17 12:22:48 +0200

    Added graph to exercise
    With dummy numbers

--- app/src/main/java/com/example/liftingstack/ExerciseActivities/ExerciseInstructionsPage.java
+++ app/src/main/java/com/example/liftingstack/ExerciseActivities/ExerciseInstructionsPage.java
@@ -3,6 +3,7 @@ package com.example.liftingstack.ExerciseActivities;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Bundle;
@@ -20,8 +21,14 @@ import com.example.liftingstack.Controller.ImageHandler;
 import com.example.liftingstack.Entity.AllExerciseInstructions;
 import com.example.liftingstack.Entity.ExerciseInstruction;
 import com.example.liftingstack.R;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class ExerciseInstructionsPage extends AppCompatActivity {
     private ImageView imageView;
@@ -64,8 +71,47 @@ public class ExerciseInstructionsPage extends AppCompatActivity {
         if (currentExerciseInstruction.getImage() != null) {
             imageView.setImageBitmap(new ImageHandler().convertBase64ToBitmap(currentExerciseInstruction.getImage()));
         }
-
+        setupGraph();
     }
+
+    public void setupGraph(){
+        LineChart lineChart = (LineChart) findViewById(R.id.linechart);
+
+        // lineChart.setOnChartGestureListener(ExerciseGraph.this);
+        // lineChart.setOnChartValueSelectedListener(ExerciseGraph.this);
+
+        lineChart.setDragEnabled(true);
+        lineChart.setScaleEnabled(false);
+
+        ArrayList<Entry> yValues = new ArrayList<>();
+
+        // test values below
+        yValues.add(new Entry(0, 60f));
+        yValues.add(new Entry(1, 50f));
+        yValues.add(new Entry(2, 70f));
+        yValues.add(new Entry(3, 30f));
+        yValues.add(new Entry(4, 50f));
+        yValues.add(new Entry(5, 60f));
+
+        LineDataSet set1 = new LineDataSet(yValues, "Weight / Reps"); // change name later
+
+        set1.setFillAlpha(110);
+
+        set1.setColor(Color.rgb(2, 206, 104)); // change to a nice blue/green shade later
+        set1.setLineWidth(3f); // makes the lines a bit thicker
+        set1.setValueTextSize(10f); // size of the text showing values in chart
+        set1.setValueTextColor(Color.rgb(160,160,160));
+        set1.setCircleColor(Color.rgb(127,255,212)); // might need to change later
+        set1.setCircleRadius(5f); // might need to change later
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set1);
+
+        LineData data = new LineData(dataSets);
+
+        lineChart.setData(data);
+    }
+
 
     public void rotateImage(View v) {
         try {
