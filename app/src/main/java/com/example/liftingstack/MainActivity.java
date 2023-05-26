c1d5527b56eb182e47566c8874220343b2e994ef
JakobEdvardsson <jakob.edvardsson@outlook.com>
2023-05-26 14:06:01 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/liftingstack/MainActivity.java
+++ app/src/main/java/com/example/liftingstack/MainActivity.java
@@ -34,6 +34,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
 
         // Check if its the first time app opens
         SharedPreferences sharedPreferences;
